71e5db240451f1cab5f6d4de4118c8dc2cebce57
ispatarov <opmopm12>
2016-06-06 18:45:10 +0300

    Home work done

---
+++ src/home_work_02_01/Main.java
@@ -0,0 +1,18 @@
+package home_work_02_01;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double a;
+		double b;
+		Scanner in = new Scanner(System.in);
+		a = in.nextDouble();
+		b = in.nextDouble();
+		double s = a * b;
+		System.out.println(s);
+
+	}
+
+}
